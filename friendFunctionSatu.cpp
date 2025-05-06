b2b758f5ce8496b55e8dc65def3ccdfbddfdbd86
ArgyTataMaulana <halmaheru37@gmail.com>
2025-05-06 19:02:29 +0700

    perubahan private class

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -2,8 +2,13 @@
 #include <string>
 using namespace std;
 class mahasiswa{
+private:
     string nama;
 public:
     friend void setName(mahasiswa &a, string);
 };
 
+void setNama(mahasiswa &a, string b){
+    
+
+}
